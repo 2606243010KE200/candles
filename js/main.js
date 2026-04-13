c36f19fe5fc8475c4a3a751bcdb8be9ae1dde613
Kira Shatalova <shatalovakira9@gmail.com>
2026-04-13 21:55:09 +0300

    Первый коммит

---
+++ js/main.js
@@ -0,0 +1,68 @@
+const menuBtn = document.querySelector(".menu__btn");
+const menuMobile = document.querySelector(".header__menu-list");
+
+menuBtn.addEventListener("click", () => {
+  menuMobile.classList.toggle("menu--open");
+});
+
+const swiperOne = new Swiper(".feedback__slider", {
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const swiperTwo = new Swiper(".cirtificates__slider", {
+  loop: true,
+  slidesPerView: "3",
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    360: {
+      slidesPerView: 1,
+    },
+  },
+});
+const accordionTitles = document.querySelectorAll(".accordion__title");
+
+accordionTitles.forEach(function (accordionTitle) {
+  accordionTitle.addEventListener("click", function () {
+    const currentText = accordionTitle.nextElementSibling;
+
+    accordionTitle.classList.toggle("accordion__title--active");
+    currentText.classList.toggle("accordion__text--visible");
+
+    if (currentText.classList.contains("accordion__text--visible")) {
+      currentText.style.maxHeight = currentText.scrollHeight + "px";
+    } else {
+      currentText.style.maxHeight = null;
+    }
+  });
+});
+/*const accordion = document.querySelector(".accordion");
+const accordionTitles = accordion.querySelectorAll(".accordion__title");
+
+accordionTitles.forEach(function (accordionTitle) {
+  accordionTitle.addEventListener("click", function () {
+    const currentText =
+      accordionTitle.parentElement.querySelector(".accordion__text");
+
+    accordionTitle.classList.toggle("accordion__title--active");
+    currentText.classList.toggle("accordion__text--visible");
+
+    if (currentText.classList.contains("accordion__text--visible")) {
+      currentText.style.maxHeight = currentText.scrollHeight + "px";
+    } else {
+      currentText.style.maxHeight = null;
+    }
+  });
+})*/
